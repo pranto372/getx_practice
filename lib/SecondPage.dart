8becead394f23ff7258b23df5f6ca9f13092ab33
pranto372 <prantodeb62@gmail.com>
2023-12-06 19:22:46 +0600

    Routing

--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_practice/SecondPage.dart';
+import 'package:getx_practice/HomePage.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page",),
+        title: const Text("Second Page",),
         backgroundColor: Colors.blue,
       ),
       body: Center(
         child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, 
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
           ),
           onPressed: (){
-            Get.to(SecondPage());
-
-            /// Get.off(SecondPage());
+            Get.offAll(HomePage());
           },
           child: const Text("Click Me", style: TextStyle(color: Colors.white),),),
       ),
