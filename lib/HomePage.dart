d68de138b6e6152e8ebf40b058f98e5e8933188f
pranto372 <prantodeb62@gmail.com>
2023-12-06 18:30:51 +0600

    SnackBar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,8 +16,23 @@ class HomePage extends StatelessWidget {
           style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, 
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
           ),
-          onPressed: (){}, 
-          child: Text("Click Me", style: TextStyle(color: Colors.white),),),
+          onPressed: (){
+            Get.snackbar(
+              "Hi", "Hello GetX",
+              snackPosition: SnackPosition.BOTTOM,
+              showProgressIndicator: true,
+              backgroundColor: Colors.blueGrey,
+              colorText: Colors.white,
+              borderRadius: 5,
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(2),
+              icon: const Icon(Icons.exit_to_app, color: Colors.white,),
+              isDismissible: false,
+              duration: const Duration(seconds: 1),
+              animationDuration: const Duration(milliseconds: 1000),
+            );
+          },
+          child: const Text("Click Me", style: TextStyle(color: Colors.white),),),
       ),
     );
   }
